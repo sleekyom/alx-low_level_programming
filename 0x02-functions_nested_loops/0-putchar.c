84b07fe97f36e10bca09ec363fc6a579fdab568f
Emmanuel Kolawole <amdas72@gmail.com>
2022-07-05 13:40:12 -0700

    put c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+ * main -  function is the entry point of the program
+ *
+ * Returns: 0 if the program ends successfully
+ */
+int main(void)
+{
+char *message = "_putchar";
+_putchar(message);
+_putchar('\n');
+return (0);
+}
